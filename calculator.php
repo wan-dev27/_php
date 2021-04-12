a74c1200173cfe6ae1e108a3177c75fbdd61e98d
wan-dev27 <xwan27@gmail.com>
2021-04-13 05:07:30 +0700

    update css

--- calculator.php
+++ calculator.php
@@ -30,61 +30,92 @@ if (isset($_POST['submit'])){
     <style>
     body{
         box-sizing: border-box;
-        padding: 0;
-        font-size: 14px;
+        background: #F2F2F2;
+	    font-family: sans-serif;
+    }
+    .calc{
+
+        width: 335px;
+        background: #2F495A;
+        margin: 100px auto;
+        padding: 10px 20px 50px 20px;
+        border-radius: 5px;
+        box-shadow: 0px 10px 20px 0px #D1D1D1;
+        
+    }
+    .numb{
+        width: 300px;
+        margin: 5px;
+        border: none;
+        font-size: 16pt;
+        border-radius: 5px;
+        padding: 10px;	
+    }
+    .opt{
+        font-size: 16pt;
+        border: none;
+        width: 215px;
+        margin: 5px;
+        border-radius: 5px;
+        padding: 10px;
+    }
+    .click{
+        background: #EC5159;
+        border-top: none;
+        border-right: none;
+        border-left: none;
+        border-radius: 5px;
+        padding: 10px 20px;
+        color: #eee;
+        font-size: 15pt;
+        border-bottom:4px solid #BF3D3D;
+    }
+    .brand{
+        color: #eee;
+        font-size: 11pt;
+        float: right;
+        text-decoration: none;
+        margin: 12px;
+    }
+    .judul{
+        text-align: center;
+	    color: #eee;
+	    font-weight: normal;
     }
     form{
         background-color: brown;
-        /* border: 1px solid black; */
+        border-radius: 5px;
+        box-shadow: 0px 10px 20px 0px #D1D1D1;
     }
-    ul li{
-        list-style: none;
-    }
-    ul li input{
-        font-size: 2em;
-        padding: 10px;
-        margin: 5px;
-    }
-    ul li select{
-        font-size: 2em;
-        color: blue;
-        background-color: burlywood;
-    }
-    ul li button{
-        font-size: 2em;
-        padding: 5px;
-    }
+    
     </style>
 </head>
 <body>
+<div class="calc">
+    <h2 class="judul">CALCULATOR</h2>
+    
+        <form action="" method="post">
 
-    <form action="" method="post">
-    <ul>
-        <li>
-            <input type="text" name="angka1" id="angka1" placeholder="Number 1">
-        </li>
-        <li>
-            <input type="text" name="angka2" id="angka2" placeholder="Number 2">
-        </li>
-
-        <li>
-            <select name="operasi" id="opt">
-                <option value="add">+</option>
-                <option value="sub">-</option>
-                <option value="multi">*</option>
-                <option value="divide">/</option>
-            </select>
-            <button type="submit" name="submit">Result</button>
-        </li>
-        <li>
-        <?php if(isset($_POST['submit'])): ?>
-			<input type="text" value="<?php echo $angka3; ?>">
-        <?php else: ?>
-			<input type="text" value="0">
-		<?php endif; ?>
-        </li>
-    </ul>
-    </form>
-
+                    <input class="numb" type="text" name="angka1" id="angka1" placeholder="Number 1" autocomplete="off">
+                    <input class="numb" type="text" name="angka2" id="angka2" placeholder="Number 2" autocomplete="off">
+                
+                    <select class="opt" name="operasi" id="opt">
+                        <option value="add">+</option>
+                        <option value="sub">-</option>
+                        <option value="multi">*</option>
+                        <option value="divide">/</option>
+                    </select>
+                    <button class="click" type="submit" name="submit">Result</button>
+                
+                <?php if(isset($_POST['submit'])): ?>
+                    <input class="numb" type="text" value="<?php echo $angka3; ?>">
+                <?php else: ?>
+                    <input class="numb" type="text" value="0">
+                <?php endif; ?>
+                
+                
+        </form>
+        <a class="brand" href="https://github.com/wan-dev27/_php">copyright&copy;wan27</a>
+    </div>
 </body>
 </html>
